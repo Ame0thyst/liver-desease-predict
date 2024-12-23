dc218271bd9623d6bd7c0073278a66f1446cbe94
Ame0thyst <kokoo15011209@gmail.com>
2024-12-23 10:44:23 +0700

    chore : pencegahan

--- page/penyakitLiver.py
+++ page/penyakitLiver.py
@@ -77,47 +77,27 @@ Hepatitis adalah peradangan pada organ hati yang disebabkan oleh infeksi virus, 
         <p style='text-align: justify;'>Cuci tangan dengan sabun dan air bersih secara teratur, terutama sebelum makan atau menyiapkan makanan.</p>
         <p style='text-align: justify;'>Hindari makan makanan atau minum air yang mungkin terkontaminasi, terutama saat bepergian ke daerah dengan sanitasi yang buruk.</p>
         </section>
+    <section>
+        <h4>3. Hindari Kontak dengan Darah dan Cairan Tubuh</h4>
+        <p style='text-align: justify;'>Gunakan sarung tangan saat memberikan pertolongan pertama atau membersihkan darah.</p>
+        <p style='text-align: justify;'>Jangan berbagi sikat gigi, alat cukur, atau barang pribadi lainnya yang mungkin terkontaminasi darah.</p>
+        </section>
+    <section>
+        h4>4. Praktik Seks yang Aman</h4>
+        <p style='text-align: justify;'>Gunakan kondom secara konsisten dan benar saat berhubungan seks.</p>
+        <p style='text-align: justify;'>Batasi jumlah pasangan seksual dan hindari berhubungan seks dengan orang yang terinfeksi hepatitis B atau C.</p>
+        </section>
+    <section>
+        h4>5. Hindari Penggunaan Narkoba Suntik</h4>
+        <p style='text-align: justify;'>Jangan pernah berbagi jarum suntik, alat suntik, atau peralatan lainnya yang digunakan untuk menyuntikkan narkoba.</p>
+        </section>
+    <section>
+        h4>6. Skrining dan Pengujian</h4>
+        <p style='text-align: justify;'>Lakukan skrining hepatitis B dan C jika Anda berisiko tinggi, seperti memiliki riwayat transfusi darah, tato, atau tindik.</p>
+        <p style='text-align: justify;'>Ibu hamil harus menjalani skrining hepatitis B untuk mencegah penularan ke bayi mereka saat lahir.</p>        
+        </section>
 ''', unsafe_allow_html=True)
-    st.markdown('''
-        <section>
-            <h3>Pencegahan Penyakit Hepatitis</h3>
-            
-            <h3>1. Vaksinasi</h3>
-            <p>
-                <span>Vaksin hepatitis A dan B tersedia dan sangat efektif dalam mencegah infeksi.</span>
-                <span>Vaksinasi direkomendasikan untuk semua bayi, anak-anak, dan orang dewasa yang berisiko tinggi.</span>
-            </p>
-
-            <h3>2. Praktik Higiene yang Baik</h3>
-            <p>
-                <span>Cuci tangan dengan sabun dan air bersih secara teratur, terutama sebelum makan atau menyiapkan makanan.</span>
-                <span>Hindari makan makanan atau minum air yang mungkin terkontaminasi, terutama saat bepergian ke daerah dengan sanitasi yang buruk.</span>
-            </p>
-
-            <h3>3. Hindari Kontak dengan Darah dan Cairan Tubuh</h3>
-            <p>
-                <span>Gunakan sarung tangan saat memberikan pertolongan pertama atau membersihkan darah.</span>
-                <span>Jangan berbagi sikat gigi, alat cukur, atau barang pribadi lainnya yang mungkin terkontaminasi darah.</span>
-            </p>
-
-            <h3>4. Praktik Seks yang Aman</h3>
-            <p>
-                <span>Gunakan kondom secara konsisten dan benar saat berhubungan seks.</span>
-                <span>Batasi jumlah pasangan seksual dan hindari berhubungan seks dengan orang yang terinfeksi hepatitis B atau C.</span>
-            </p>
-
-            <h3>5. Hindari Penggunaan Narkoba Suntik</h3>
-            <p>
-                <span>Jangan pernah berbagi jarum suntik, alat suntik, atau peralatan lainnya yang digunakan untuk menyuntikkan narkoba.</span>
-            </p>
-
-            <h3>6. Skrining dan Pengujian</h3>
-            <p>
-                <span>Lakukan skrining hepatitis B dan C jika Anda berisiko tinggi, seperti memiliki riwayat transfusi darah, tato, atau tindik.</span>
-                <span>Ibu hamil harus menjalani skrining hepatitis B untuk mencegah penularan ke bayi mereka saat lahir.</span>
-            </p>
-            </section>
-''', unsafe_allow_html=True)
+    
 
 
             # st.markdown('''
